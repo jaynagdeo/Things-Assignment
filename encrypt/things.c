94a855c7bebb6dfd4b368c98b82fcf9059a86a67
jaynagdeo <jaynagdeo20@gmail.com>
2017-11-11 23:47:02 +0100

    cleaner code

--- encrypt/things.c
+++ encrypt/things.c
@@ -2,157 +2,99 @@
 #include<stdlib.h>
 #include "mbedtls/config.h"
 #include "mbedtls/aes.h"
-
+#include <string.h>
 
 int main(int argc, unsigned char const *argv[]) {
-  /* code */
-char *p;
-unsigned int i=0,ret=0, mode=1,n;
 
-unsigned long int NwkKey=0;
+char *p;
+unsigned int i=0,mode=1;
 
 unsigned long int NwkKey1=0;
 unsigned long int NwkKey2=0;
-// 0x01 // 1 char
-unsigned long int JoinNonce=0; // 3 chars
-unsigned long int JoinEUI=0;  // 8 chars
-unsigned long int DevNonce=0; // 2 chars
-unsigned char iv[16];
-unsigned char *tempNwkKey;  // using character pointers to access bytes from a long integer
+// 0x01 // 1 byte
+unsigned long int JoinNonce=0; // 3 bytes
+unsigned long int JoinEUI=0;  // 8 bytes
+unsigned long int DevNonce=0; // 2 bytes
+
+// using character pointers to access bytes from a long integer
 unsigned char *tempNwkKey1;
 unsigned char *tempNwkKey2;
-unsigned char *tempJoinNonce;
-unsigned char *tempJoinEUI;
-unsigned char *tempDevNonce;
-tempNwkKey=&NwkKey;
 tempNwkKey1=&NwkKey1;
 tempNwkKey2=&NwkKey2;
-tempJoinNonce=&JoinNonce;
-tempJoinEUI=&JoinEUI;
-tempDevNonce=&DevNonce;
-
 
 mbedtls_aes_context aes;
 
+unsigned char key[16]; // NwkKey
 
-unsigned char longkey[32];
-unsigned char longkey1[16];
-unsigned char longkey2[16];
-unsigned char key[16];
+unsigned char * lk1 = malloc(16 * sizeof(char));
+unsigned char * lk2 = malloc(16 * sizeof(char));
 
-unsigned char * lk1 = malloc(3 * sizeof(char));
-unsigned char * lk2 = malloc(3 * sizeof(char));
-
-unsigned char input [16];
-
-unsigned char output[16];
+unsigned char input [16]; //JoinNonce+JoinEUI+DevNonce
+unsigned char output[16]; // Output of encryption
 
 mbedtls_aes_init( &aes );
 
-JoinNonce=strtoul(argv[2], &p, 16);
+JoinNonce=strtoul(argv[2], &p, 16); //converting to HEX
 JoinEUI=strtoul(argv[3], &p, 16);
 DevNonce=strtoul(argv[4], &p, 16);
 
-memcpy( longkey, argv[1], 32 );
+//splitting the NwkKey into two parts
+memcpy( lk1, argv[1], 16 );
+memcpy( lk2, (argv[1]+16), 16 );
 
-// printf("Longkey is: %s\n", longkey);
-
-
-
-memcpy( lk1, longkey, 16 ); //splitting the key into two parts as the strtoul function is not able to process more than 8 bytes
-memcpy( lk2, (longkey+16), 16 );
-
-
-// printf("Longkey1 is: %s\n", lk1);
-// printf("Longkey2 is: %s\n", lk2);
-
-NwkKey1=strtoul(lk1, &p, 16);
+NwkKey1=strtoul(lk1, &p, 16); //converting to HEX
 NwkKey2=strtoul(lk2, &p, 16);
-
 
 for (i=0;i<8;i++)
   {
-    key[7-i]=*(tempNwkKey1+i);   //merging the results from two strtoul functions into one
+    //merging the results from two strtoul functions into one
+    key[7-i]=*(tempNwkKey1+i);
     key[15-i]=*(tempNwkKey2+i);
   }
+
 printf("\n\n");
 printf("The NwkKey is: ");
   for (i=0;i<16;i++)
     {
-      printf("%02x", key[i]);
+      printf("%02X", key[i]);
     }
 printf("\n\n");
 
-input[0]=0x01;
+input[0]=0x01; // for FNwkSIntKey
 
 memcpy( input + 1, &JoinNonce, 3 );
 memcpy( input + 4, &JoinEUI, 8 );
 memcpy( input + 12, &DevNonce, 2 );
-input[14]=0x00;
+input[14]=0x00; //zero padding
 input[15]=0x00;
-
-  // for (i=0;i<16;i++)
-  //   {
-  //     key[i]=*(tempNwkKey+i);
-  //     printf("%c", key[i]);
-  //   }
-  //   printf("\n");
-/*
-  input[0]=0x03;
-//
-for (i=0;i<3;i++)
-  {
-    input[3-i]=*(tempJoinNonce+i);
-    //printf("%x", input[i]);
-  }
-
-  for (i=0;i<8;i++)
-    {
-      input[11-i]=*(tempJoinEUI+i);
-    //  printf("%x\n", iv[i+3+1]);
-    }
-//
-    for (i=0;i<2;i++)
-      {
-        input[13-i]=*(tempDevNonce+i);
-        //printf("%x\n", iv[i+3+8]);
-      }
-input[14]=0;
-input[15]=0;   // zero padding
-
-printf("\n\n");*/
 
 printf("The input is: ");
   for (i=0;i<16;i++)
     {
-      printf("%x", input[i]);
+      printf("%02X", input[i]);
     }
 printf("\n\n");
 
 
-ret = mbedtls_aes_setkey_enc( &aes, key, 128 );
+mbedtls_aes_setkey_enc( &aes, key, 128 );
 
-// // int mbedtls_aes_crypt_ecb( mbedtls_aes_context *ctx,
-// //                     int mode,
-// //                     const unsigned char input[16],
-// //                     unsigned char output[16] );
-//
+//AES encryption
 mbedtls_aes_crypt_ecb( &aes, mode, input, output);
 
-//mbedtls_aes_encrypt(&aes, input, output);
 
 printf("FNwkSIntKey: ");
  for (i=0;i<16;i++)
-   printf("%x", output[i]);
+   printf("%02X", output[i]);
 printf("\n\n");
 
-input[0]=0x03;
+input[0]=0x03; //For SNwkSIntKey, rest of the data is same
 
+//AES encryption
 mbedtls_aes_crypt_ecb( &aes, mode, input, output);
 
 printf("SNwkSIntKey: ");
  for (i=0;i<16;i++)
-   printf("%x", output[i]);
+   printf("%02X", output[i]);
 printf("\n\n");
 
 mbedtls_aes_free( &aes );
